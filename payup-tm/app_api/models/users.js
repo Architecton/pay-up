93da3d0a788108bb02019bb62f272f2cb96a0299
Jernej Vivod <jv6916@student.uni-lj.si>
2018-12-06 18:40:44 +0000

    Update loan schema

--- payup-tm/app_api/models/users.js
+++ payup-tm/app_api/models/users.js
@@ -1,5 +1,34 @@
 var mongoose = require("mongoose");
 
+// Schema representing a loan
+var loanShema = new mongoose.Schema({
+  loaner: {type: String, required: true},           // Loaner's username (pk)
+  recipient: {type: String, required: true},        // Recipient's username (pk)
+  dateIssued: {type: Date, "default": Date.now()},  // Date the loan was issued
+  deadline: {type: Date, required: true},           // Date the loan must be repaid
+  amount: {type: Number, required: true},           // Loan amount
+  currency: {type: String, required: true},         // Loan currency
+  interest: {type: Number, required: true},         // Interest rate (yearly)
+  compoundInterest: {type: Boolean, "default": false, required: true}, // false ~ simple interest; true ~ interest on interest
+  status: {type: Number, "default": 0, required: true} // 0 ~ pending; 1 ~ active; 2 ~ resolved
+});
+
+// TODO
+// Schema representing a contact
+var contactSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  surname: {type: String, required: true},
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  email: {type: String, required: true},
+  gender: {type: String, required: true},
+  dateJoined: {type: Date, "default": Date.now()},
+  status: {type: Number, "default": 0},                  // 0 ~ activated_no; 1 ~ activated_yes; 2 ~ terminated
+  proDefauld: {type: Boolean, "default": false},         // false ~ default; true ~ pro
+  defaultCurrency: {type: String, "default": "EUR"},
+  nightmode: {type: Boolean, "default": false}           // false ~ off; true ~ on
+});
+
 // Schema representing a user
 var userShema = new mongoose.Schema({
   name: {type: String, required: true},
@@ -13,7 +42,11 @@ var userShema = new mongoose.Schema({
   proDefauld: {type: Boolean, "default": false},         // false ~ default; true ~ pro
   defaultCurrency: {type: String, "default": "EUR"},
   nightmode: {type: Boolean, "default": false}           // false ~ off; true ~ on
+  // TODO avatar: {type: Buffer, "defa"}
 });
+
+
+
 
 // Compile the schema into a model.
 // Name of model, schema to be used, optional name of the mongoDB collection
