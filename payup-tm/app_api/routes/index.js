e4f92978e3b280df256f5c0eaf2e0113ff951bd8
Jernej Vivod <jv6916@student.uni-lj.si>
2018-12-06 09:44:25 +0000

    Add request handler and comments

--- payup-tm/app_api/routes/index.js
+++ payup-tm/app_api/routes/index.js
@@ -1,11 +1,20 @@
 var express = require('express');
+// Create a router.
 var router = express.Router();
+// loans controller
 var ctrlLoans = require('../controllers/loans');
+var ctrlUsers = require('../controllers/users');
 
 /* GET home page. */
 router.get('/', ctrlLoans.index);
 
-// Dodana funkcija ki kreira nov Loan z POST.
-router.post('/loans', ctrlLoans.loansKreiraj);
 
+// HTTP REQUEST HANDLERS
+
+// Function that handles post requests to loans controller.
+router.post('/loans', ctrlLoans.loansCreate);
+// Function that handles get requests to loans controller.
+router.get('/loans', ctrlLoans.index);
+
+// Expose router as module.
 module.exports = router;
