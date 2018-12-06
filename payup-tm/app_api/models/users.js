698b06c6da5c69950083450e668c0bc184f66637
Jernej Vivod <jv6916@student.uni-lj.si>
2018-12-06 20:03:51 +0000

    Remove redundant file

--- payup-tm/app_api/models/users.js
+++ payup-tm/app_api/models/users.js
@@ -21,10 +21,10 @@ var loanShema = new mongoose.Schema({
 
 // Schema representing a contact
 var contactSchema = new mongoose.Schema({
-  name: {type: String, required: true},               // Name of the contact
-  surname: {type: String, required: true},            // Last name of the contact
+  name: {type: String, required: true},                  // Name of the contact
+  surname: {type: String, required: true},               // Last name of the contact
   username: {type: String, required: true, unique: true}, // username of the contact (needed for linking with actual user)
-  email: {type: String, required: true},              // contact's email
+  email: {type: String, required: true},                  // contact's email
 });
 
 
