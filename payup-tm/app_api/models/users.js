61e82389d4b6faaccb04b0b0f2503882266f0366
Jernej Vivod <jv6916@student.uni-lj.si>
2018-12-06 17:57:58 +0000

    Fix user schema

--- payup-tm/app_api/models/users.js
+++ payup-tm/app_api/models/users.js
@@ -6,13 +6,15 @@ var userShema = new mongoose.Schema({
   surname: {type: String, required: true},
   username: {type: String, required: true, unique: true},
   password: {type: String, required: true},
-  email: {type: Email, required: true},
+  email: {type: String, required: true},
   gender: {type: String, required: true},
   dateJoined: {type: Date, "default": Date.now()},
-  status: {type: Number, "default": 0}, // 0 ~ activated_no; 1 ~ activated_yes; 2 ~ terminated
-  proDefauld: {type: Boolean, "default": false}, // false ~ default; true ~ pro
+  status: {type: Number, "default": 0},                  // 0 ~ activated_no; 1 ~ activated_yes; 2 ~ terminated
+  proDefauld: {type: Boolean, "default": false},         // false ~ default; true ~ pro
   defaultCurrency: {type: String, "default": "EUR"},
-  nightmode: {type: Boolean, "default": false} // false ~ off; true ~ on
+  nightmode: {type: Boolean, "default": false}           // false ~ off; true ~ on
 });
 
+// Compile the schema into a model.
+// Name of model, schema to be used, optional name of the mongoDB collection
 mongoose.model('User', userShema, 'Users');
