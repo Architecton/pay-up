4773b5d6e4c5662306d2ff254ef456d0e5ab1ad6
Jernej Vivod <jv6916@student.uni-lj.si>
2018-12-21 20:17:18 +0000

    Continue work on SPA

--- payup-tm/app_client/app.js
+++ payup-tm/app_client/app.js
@@ -1,2 +1,13 @@
 /* global angular */
 var payupApp = angular.module('payup', []);
+
+// Add path specification as a setting of the application
+
+function setting($routeProvider) {
+  $routeProvider
+    .when('/', {})                  // When request /, go to root.
+    .otherwise({redirecTo: '/'});   // When request anything else, also redirect to root.
+}
+
+payupApp
+  .config(['$routeProvider', setting]);
