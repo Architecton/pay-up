e3e02d4322c4993ba89d0617d190eae7730fcb20
Jernej Vivod <jv6916@student.uni-lj.si>
2018-12-22 16:17:48 +0000

    Add authentication services

---
+++ payup-tm/app_client/authentication/signup/signup.controller.js
@@ -0,0 +1,49 @@
+// signupCtrl: controller that handles the sign up
+(function() {
+    function signupCtrl($location, authentication) {
+      var vm = this;
+    
+      // TODO add thata required by the API.
+      vm.signupData = {
+        ime: "",
+        elektronskiNaslov: "",
+        geslo: ""
+      };
+    
+      vm.homePage = $location.search().page || '/';
+    
+      // sendData: send sign up data to API
+      vm.sendData = function() {
+        vm.formError = "";
+        // TODO
+        if (!vm.signupData.ime || !vm.signupData.elektronskiNaslov || !vm.signupData.geslo) {
+          vm.formError = "Please fill out all the forms.";
+          return false;
+        } else {
+          vm.performSignup();
+        }
+      };
+      
+      // performSignup: perform sign up
+      vm.performSignup = function() {
+        vm.formError = "";
+        authentication    // Send POST request with sign up data.
+          .signup(vm.signupData)
+          .then(          // When finished
+            function(success) {
+              $location.search('page', null);
+              $location.path(vm.homePage);
+            },
+            function(error) {     // Error
+              vm.formError = error.data.message;
+            }
+          );
+      };
+    }
+    
+  /* global angular */
+  angular
+    .module('payupApp')
+    .controller('signupCtrl', signupCtrl);
+})();
+
--- payup-tm/app_client/common/services/authentication.service.js
+++ payup-tm/app_client/common/services/authentication.service.js
@@ -1,69 +1,86 @@
 // authentication service
 
 // authentication:
-function authentication($window, $http) {
-  var saveToken = function(token) {
-    $window.localStorage['payup-token'] = token;
-  };
-
-  // getToken: get JWT token from local storage.
-  var getToken = function() {
-    $window.localStorage['payup-token'];
-  };
-
-  // signup: make a post request to sign up
-  var signUp = function(user) {
-    return $http.post('/api/signUp', user).then(  // WARNING!!! API DOES NOT return the JWT token on sign up. Have to log in after signing up
-      function success(response) {
-        saveToken(response.data.token);
-      });
-  };
-
-  // logIn: make a request to log in. If successful, save token in local storage
-  var logIn = function(user) {
-    return $http.post('/api/logIn', user).then(
-      function success(response) {
-        saveToken(response.data.token);
-      });
-  };
-
-  // logOut: remove JWT token from local storage
-  var logOut = function() {
-    $window.localStorage.removeItem('payup-token');
-  };
-
-  // isLoggedIn: check if user is logged in
-  var isLoggedIn = function() {
-    var token = getToken();   // Try to get token.
-    if (token) {  // If token exists, get payload.
-      var payload = JSON.parse($window.atob(token.split('.')[1]));
-      return payload.expirationDate > Date.now() / 1000;  // Check whether the token has expired.
-    } else {
-      return false;     // If no token found, return false.
-    }
-  };
-
-  // currentUser: get user that is currently logged into the application (get info from JWT payload)
-  var currentUser = function() {
-    if (isLoggedIn()) {       // Check if user is logged in.
-      var token = getToken();
-      var payload = JSON.parse($window.atob(token.split('.')[1]));
-      return {  // Return username, email and name as decoded from token.
-        username: payload.username,
-        name: payload.name,
-        email: payload.email
-      };
-    }
-  };
-
-  // Return object containing the functions offered by this service.
-  return {
-    saveToken: saveToken,
-    getToken: getToken,
-    signUp: signUp,
-    logIn: logIn,
-    logOut: logOut,
-    isLoggedIn: isLoggedIn,
-    currentUser: currentUser
-  };
-}
+(function() {
+  function authentication($window, $http) {
+    
+    // Fix errors regarding UTF-8 coding.
+    var b64Utf8 = function (niz) {
+      return decodeURIComponent(Array.prototype.map.call($window.atob(niz), function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+    };
+    
+    // saveToken: save token in local storage.
+    var saveToken = function(token) {
+      $window.localStorage['payup-token'] = token;
+    };
+  
+    // getToken: get JWT token from local storage.
+    var getToken = function() {
+      $window.localStorage['payup-token'];
+    };
+  
+    // signup: make a post request to sign up
+    var signUp = function(user) {
+      return $http.post('/api/signUp', user).then(  // WARNING!!! API DOES NOT return the JWT token on sign up. Have to log in after signing up
+        function success(response) {
+          saveToken(response.data.token);
+        });
+    };
+  
+    // logIn: make a request to log in. If successful, save token in local storage
+    var logIn = function(user) {
+      return $http.post('/api/logIn', user).then(
+        function success(response) {
+          saveToken(response.data.token);
+        });
+    };
+  
+    // logOut: remove JWT token from local storage
+    var logOut = function() {
+      $window.localStorage.removeItem('payup-token');
+    };
+  
+    // isLoggedIn: check if user is logged in
+    var isLoggedIn = function() {
+      var token = getToken();   // Try to get token.
+      if (token) {  // If token exists, get payload.
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+        return payload.expirationDate > Date.now() / 1000;  // Check whether the token has expired.
+      } else {
+        return false;     // If no token found, return false.
+      }
+    };
+  
+    // currentUser: get user that is currently logged into the application (get info from JWT payload)
+    var currentUser = function() {
+      if (isLoggedIn()) {       // Check if user is logged in.
+        var token = getToken();
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+        return {  // Return username, email and name as decoded from token.
+          username: payload.username,
+          name: payload.name,
+          email: payload.email
+        };
+      }
+    };
+  
+    // Return object containing the functions offered by this service.
+    return {
+      saveToken: saveToken,
+      getToken: getToken,
+      signUp: signUp,
+      logIn: logIn,
+      logOut: logOut,
+      isLoggedIn: isLoggedIn,
+      currentUser: currentUser
+    };
+  }
+  
+  // Register service
+  /* global angular */
+  angular
+    .module('payupApp')
+    .service('authentication', authentication);
+})();
